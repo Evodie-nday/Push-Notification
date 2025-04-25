b72bae71e397eb4b9c6f2c88c2772d2b0d30a55d
NDAYISHIMIYE Evodie <ndayishimiyeevodie@gmail.com>
2025-04-25 14:39:31 -0700

    added two new classes SMS and Email notification

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         NotificationService push = new PushNotification();
+        NotificationService email = new EmailNotification();
+        NotificationService SMS = new SMSNotification();
         push.sendNotification("You have a new message.");
+        email.sendNotification("You have new Unread email.");
+        SMS.sendNotification("You have new Unread SMS.");
     }
 }
